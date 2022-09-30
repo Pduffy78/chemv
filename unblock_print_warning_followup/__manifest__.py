a63362fbfdc63b5bec7a2d716c42a55d8f39bb3e
nilesh <murray@paceautoservices.co.za>
2022-09-30 13:42:17 +0530

    [IMP]Added module to fix follow up report issue

---
+++ unblock_print_warning_followup/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Unblock Print Warning For FollowUP",
+
+    'summary': """
+    Unblock Print Warning For FollowUP
+        """,
+
+    'description': """
+        Unblock Print Warning For FollowUP
+    """,
+
+    'author': "AP",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account_followup'],
+
+    # always loaded
+    'data': [
+        
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        
+    ],
+}
