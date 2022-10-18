199dfab2349290bb78b78a4943270ae948dff3b2
nilesh <murray@paceautoservices.co.za>
2022-10-18 14:27:17 +0530

    [FIX]Fixed outstanding base64 issue

--- partner_statement/wizard/outstanding_statement_wizard.py
+++ partner_statement/wizard/outstanding_statement_wizard.py
@@ -1,7 +1,9 @@
 # Copyright 2018 ForgeFlow, S.L. (http://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from dateutil.relativedelta import relativedelta
+import base64
+from odoo import api, fields, models
 
 
 class OutstandingStatementWizard(models.TransientModel):
