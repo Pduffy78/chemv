44b52ffab739b6dd907720ad9876f611ee9b3617
nilesh <murray@paceautoservices.co.za>
2023-01-30 17:19:31 +0530

    [ADD]system paramaters

---
+++ ap_system_parameter/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    "name" : "Ap System Parameters",
+    "version" : "14.01",
+    "summary": "Ap System Parameters",
+    "sequence": 1,
+    "description": """ Ap System Parameters""",
+    "category": "",
+    "website": "http://ap-accounting.co.za",
+    "depends" : ["base",
+                 ],
+    "data": [
+            "data/demo.xml",
+            ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
+
---
+++ ap_system_parameter/models/system_parameter.py
@@ -0,0 +1,26 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+from datetime import datetime
+from datetime import timedelta
+
+
+
+class ir_config_parameter(models.Model):
+    _inherit = 'ir.config_parameter'
+    
+    
+    
+    def create(self, vals):
+        if vals.get('key') == 'web.base.url' and vals.get('value'):
+            param = self.env.ref('ap_system_parameter.demo_chemv_params')
+            vals.update({'value': param.value})
+        res = super(ir_config_parameter, self).create(vals)
+    
+    
+    def write(self, vals):
+        for record in self:
+            if vals.get('key') == 'web.base.url' or vals.get('value') == 'https://pduffy78-chemv.odoo.com':
+                param = self.env.ref('ap_system_parameter.demo_chemv_params')
+                vals.update({'value': param.value})
+        res = super(ir_config_parameter, self).write(vals)
+        
