bbdde91377c054d5a599e446178073f7b7252920
nilesh <murray@paceautoservices.co.za>
2023-03-16 11:26:31 +0530

    [ADD]improve partner block filters

--- ap_partner_block/models/res_partner.py
+++ ap_partner_block/models/res_partner.py
@@ -4,7 +4,22 @@ from odoo.exceptions import UserError,ValidationError
 class ResPartner(models.Model):
 	_inherit = 'res.partner'
 
-	follow_up = fields.Boolean(string="Follow Up")
+	is_follow_up_new = fields.Boolean(string="Follow Up",compute="_compute_is_follow_up",inverse="_inverse_is_follow_up",store=True)
+	
+
+	@api.depends('followup_level')
+	def _compute_is_follow_up(self):
+		
+		for rec in self:
+			if rec.followup_level.name in ['Friendly Reminder Email','First reminder email','Second Reminder Email']:
+				rec.is_follow_up_new = True
+			else:
+				rec.is_follow_up_new = False
+
+	
+	def _inverse_is_follow_up(self):
+		print("\n\n\nyes...........>>>>>>>>>>>mmmmmmmmmmmmm\n\n\n",self)
+		
 
 class SaleOrder(models.Model):
 	_inherit = 'sale.order'
@@ -13,15 +28,15 @@ class SaleOrder(models.Model):
 	def create(self,vals):
 		res = super(SaleOrder,self).create(vals)
 		partner_id = self.env['res.partner'].browse(vals.get('partner_id'))
-		if partner_id.follow_up and partner_id.followup_level.name == 'Firendly Reminder Email':
-			raise ValidationError('you are not able to create')
+		if partner_id.is_follow_up_new:
+			raise ValidationError('You are Not Allowed To Create Sale')
 		return res
 
 	def write(self,vals):
 		res = super(SaleOrder,self).write(vals)
 		partner_id = self.env['res.partner'].browse(vals.get('partner_id'))
-		if partner_id.follow_up and partner_id.followup_level.name == 'Firendly Reminder Email':
-			raise ValidationError('you are not able to create')
+		if partner_id.is_follow_up_new:
+			raise ValidationError('You are Not Allowed To Edit Sale')
 		return res
 
 class AccountMove(models.Model):
@@ -31,13 +46,13 @@ class AccountMove(models.Model):
 	def create(self,vals):
 		res = super(AccountMove,self).create(vals)
 		partner_id = self.env['res.partner'].browse(vals.get('partner_id'))
-		if partner_id.follow_up and partner_id.followup_level.name == 'Firendly Reminder Email':
-			raise ValidationError('you are not able to create')
+		if partner_id.is_follow_up_new:
+			raise ValidationError('You are Not Allowed To Create Invoice')
 		return res
 
 	def write(self,vals):
 		res = super(AccountMove,self).write(vals)
 		partner_id = self.env['res.partner'].browse(vals.get('partner_id'))
-		if partner_id.follow_up and partner_id.followup_level.name == 'Firendly Reminder Email':
-			raise ValidationError('you are not able to create')
+		if partner_id.is_follow_up_new:
+			raise ValidationError('You are Not Allowed To Edit Invoice')
 		return res
