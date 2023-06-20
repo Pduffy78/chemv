8d2e25a61f882da013f1345732b2cdde50865272
nilesh <murray@paceautoservices.co.za>
2023-06-20 18:07:51 +0530

    [ADD]Added payfast module

---
+++ payment_payfast_73lines/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo Module Developed by 73lines
+# See LICENSE file for full copyright and licensing details.
+
+""" Import models and controllers """
+
+from . import models
+from . import controllers
+# from odoo.addons.payment.models.payment_acquirer import create_missing_journal_for_acquirers
+
