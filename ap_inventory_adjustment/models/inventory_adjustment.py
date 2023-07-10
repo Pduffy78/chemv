5357e2fccca5f3451748ff061237de83956fb2f1
nilesh <murray@paceautoservices.co.za>
2023-07-10 18:18:25 +0530

    [tst]

--- ap_inventory_adjustment/models/inventory_adjustment.py
+++ ap_inventory_adjustment/models/inventory_adjustment.py
@@ -173,6 +173,4 @@ class stock_quant(models.Model):
     def create(self, vals):
         res = super(stock_quant, self).create(vals)
         res._onchange_location_or_product_id()
-        if self._is_inventory_mode():
-            res._check_company()
         return res
