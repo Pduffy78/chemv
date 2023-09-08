6d2d50e80dda57c1ec9e188109bc917a60737d29
nilesh <murray@paceautoservices.co.za>
2023-09-08 15:11:10 +0530

    [ADD]Added payment date module invoice screen update code

--- latest_payment_date/models/payment_date.py
+++ latest_payment_date/models/payment_date.py
@@ -11,14 +11,14 @@ class AccountMove(models.Model):
     @api.depends('move_type', 'line_ids.amount_residual','payment_state', 'update_payment_date')
     def _compute_latest_payment_date(self):
         for move in self:
-            # list_date = []
-            # payments_widget_vals = {'title': _('Less Payment'), 'outstanding': False, 'content': []}
-            # if move.move_type == 'out_invoice' and  move.state == 'posted' and move.is_invoice(include_receipts=True):
-            #     for data in move._get_reconciled_info_JSON_values():
-            #         list_date.append(data.get('date'))
-            #     if list_date:
-            #         move.latest_paymn_date = max(list_date)
-            # else:
+            list_date = []
+            payments_widget_vals = {'title': _('Less Payment'), 'outstanding': False, 'content': []}
+            if move.move_type == 'out_invoice' and  move.state == 'posted' and move.is_invoice(include_receipts=True):
+                for data in move._get_reconciled_info_JSON_values():
+                    list_date.append(data.get('date'))
+                if list_date:
+                    move.latest_paymn_date = max(list_date)
+            else:
                 move.latest_paymn_date = False
     
     
