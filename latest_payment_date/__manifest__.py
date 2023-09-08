069583c1d36f0baf4bacb6cb317c52c522cecdb6
nilesh <murray@paceautoservices.co.za>
2023-09-08 14:48:22 +0530

    [ADD]Added payment date module invoice screen

---
+++ latest_payment_date/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Latest Payment Date",
+
+    'summary': "",
+
+    'description': "",
+
+    'author': "AP Accounting",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Account',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/payment_date.xml',
+    ],
+    'demo': [
+    ],
+}
