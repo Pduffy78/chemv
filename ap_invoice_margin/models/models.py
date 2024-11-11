bdee9e7c05df3f7881b982acc1b3a3d74fcf0011
nileshmh34 <you@nilesh.galoriya@gmail.com>
2024-11-11 18:59:44 +0530

    [ADD]Added code for margin

--- ap_invoice_margin/models/models.py
+++ ap_invoice_margin/models/models.py
@@ -34,13 +34,13 @@ class ap_invoice_margin(models.Model):
 
     margin = fields.Float(
         "Margin", compute='_compute_margin',
-        digits='Product Price', store=True, precompute=True)
+        digits='Product Price', store=False, precompute=True)
     margin_percent = fields.Float(
-        "Margin (%)", compute='_compute_margin', store=True)
+        "Margin (%)", compute='_compute_margin', store=False, precompute=True)
 
     purchase_price = fields.Float(
         string="Cost", compute="_compute_purchase_price",
-        digits='Product Price', store=True, readonly=False, copy=False)
+        digits='Product Price', store=False, readonly=False, copy=False, precompute=True)
 
     @api.depends('product_id', 'company_id', 'currency_id', 'product_uom_id')
     def _compute_purchase_price(self):
