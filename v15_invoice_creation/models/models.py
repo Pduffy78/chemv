0c69076297311dc534dcf8cbd503b1d33809e3e4
nileshmh34 <89900188+nileshmh34@users.noreply.github.com>
2025-07-18 12:17:18 +0530

    [FIX]

--- v15_invoice_creation/models/models.py
+++ v15_invoice_creation/models/models.py
@@ -48,37 +48,83 @@ class ResUsers(models.Model):
 
     _inherit = 'res.users'
 
-    @api.model
+    # @api.model
+    # def has_group(self, group_ext_id: str) -> bool:
+    #     # use singleton's id if called on a non-empty recordset, otherwise
+    #     # context uid
+
+    #     uid = self.id or self._uid
+    #     user_group = [data.name for data in self.env.user.groups_id if data.name=='Sale Invoice Confirmation']
+    #     if group_ext_id in ['account.group_account_manager','account.group_account_invoice']   and user_group:
+    #         return True
+    #     return self.with_user(uid)._has_group(group_ext_id)
+
+    @api.readonly
     def has_group(self, group_ext_id: str) -> bool:
-        # use singleton's id if called on a non-empty recordset, otherwise
-        # context uid
+        """ Return whether user ``self`` belongs to the given group (given by its
+        fully-qualified external ID).
+
+        Note that the group ``"base.group_no_one"`` is only effective in debug
+        mode: the method returns ``True`` if the user belongs to the group and
+        the current request is in debug mode.
+        """
+
+
+        self.ensure_one()
+        if not (self.env.su or self == self.env.user or self.env.user._has_group('base.group_user')):
+            # this prevents RPC calls from non-internal users to retrieve
+            # information about other users
+            raise AccessError(_("You can ony call user.has_group() with your current user."))
+
+        result = self._has_group(group_ext_id)
+        if group_ext_id == 'base.group_no_one':
+            result = result and bool(request and request.session.debug)
+        # uid = self.id or self._uid
 
         uid = self.id or self._uid
         user_group = [data.name for data in self.env.user.groups_id if data.name=='Sale Invoice Confirmation']
-        if group_ext_id in ['account.group_account_manager','account.group_account_invoice']   and user_group:
+        if group_ext_id in ['account.group_account_manager','account.group_account_invoice'] and user_group:
             return True
-        return self.with_user(uid)._has_group(group_ext_id)
+        return result
 
-    @api.model
-    @tools.ormcache('self._uid', 'group_ext_id')
-    def _has_group(self, group_ext_id):
+
+    def _has_group(self, group_ext_id: str) -> bool:
         user_group = [data.name for data in self.env.user.groups_id if data.name=='Sale Invoice Confirmation']
         if group_ext_id == 'account.group_account_manager' and user_group:
             return True
-
-        """Checks whether user belongs to given group.
+        """ Return whether user ``self`` belongs to the given group.
 
         :param str group_ext_id: external ID (XML ID) of the group.
            Must be provided in fully-qualified form (``module.ext_id``), as there
            is no implicit module to use..
-        :return: True if the current user is a member of the group with the
+        :return: True if user ``self`` is a member of the group with the
            given external ID (XML ID), else False.
         """
-        assert group_ext_id and '.' in group_ext_id, "External ID '%s' must be fully qualified" % group_ext_id
-        module, ext_id = group_ext_id.split('.')
-        self._cr.execute("""SELECT 1 FROM res_groups_users_rel WHERE uid=%s AND gid IN
-                            (SELECT res_id FROM ir_model_data WHERE module=%s AND name=%s)""",
-                         (self._uid, module, ext_id))
-        return bool(self._cr.fetchone())
-    has_group.clear_cache = _has_group.clear_cache
+        group_id = self.env['res.groups']._get_group_definitions().get_id(group_ext_id)
+        # for new record don't fill the ormcache
+        return group_id in (self._get_group_ids() if self.id else self.groups_id._origin._ids)
+
+    # @api.model
+    # @tools.ormcache('self._uid', 'group_ext_id')
+    # def _has_group(self, group_ext_id):
+    #     user_group = [data.name for data in self.env.user.groups_id if data.name=='Sale Invoice Confirmation']
+    #     if group_ext_id == 'account.group_account_manager' and user_group:
+    #         return True
+
+    #     """Checks whether user belongs to given group.
+
+    #     :param str group_ext_id: external ID (XML ID) of the group.
+    #        Must be provided in fully-qualified form (``module.ext_id``), as there
+    #        is no implicit module to use..
+    #     :return: True if the current user is a member of the group with the
+    #        given external ID (XML ID), else False.
+    #     """
+    #     assert group_ext_id and '.' in group_ext_id, "External ID '%s' must be fully qualified" % group_ext_id
+    #     module, ext_id = group_ext_id.split('.')
+    #     self._cr.execute("""SELECT 1 FROM res_groups_users_rel WHERE uid=%s AND gid IN
+    #                         (SELECT res_id FROM ir_model_data WHERE module=%s AND name=%s)""",
+    #                      (self._uid, module, ext_id))
+    #     return bool(self._cr.fetchone())
+
+    # has_group.clear_cache = _has_group.clear_cache
     
